da71da174f40076cffb9d4618a1fc57380cf003e
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-06-25 19:12:16 +0400

    Adds default styles.

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './_template.css';
 
 export default class Template extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <div>
+      <div className="container">
         {children}
       </div>
     );
