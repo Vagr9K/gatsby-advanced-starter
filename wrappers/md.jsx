27cabcf01b2343a445ad63976d1f4399cc138256
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-06-29 14:53:11 +0400

    Adds Markdown output styling.

--- wrappers/md.jsx
+++ wrappers/md.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import Helmet from 'react-helmet';
+import { config } from 'config';
+import Card from 'react-md/lib/Cards';
+import './atom-one-dark.css';
+import './md.scss';
 
 export default class MarkdownWrapper extends React.Component {
   render() {
     const post = this.props.route.page.data;
     return (
-      <div className="markdown">
-        <h1>{post.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.body }} />
+      <div className="md-grid post-container">
+
+        <Helmet
+          title={`${post.title} | ${config.siteTitle}`}
+        />
+        <Card className="md-grid md-cell md-cell--12 post">
+          <div className="markdown md-typography-text-container post-body">
+            <h1>{post.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: post.body }} />
+          </div>
+        </Card>
       </div>
     );
   }
