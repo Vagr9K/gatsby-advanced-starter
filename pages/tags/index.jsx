ef20b2de2d6a76c3ac610684a9f5f97fa475ac75
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-02 16:54:07 +0400

    Adds React component skeleton to the tag page.

---
+++ pages/tags/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class TagsPage extends Component {
+  render() {
+    return (
+      <div className="tags-container" />
+    );
+  }
+}
+
+export default TagsPage;
