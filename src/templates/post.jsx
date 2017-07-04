46d7ab431eaffc813e5c20b1ad8b6decb460dbe3
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-04 22:59:13 +0400

    Adds layout to post views.

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Card from 'react-md/lib/Cards';
 import CardText from 'react-md/lib/Cards/CardText';
+import MainLayout from '../components/MainLayout/MainLayout.jsx';
 import UserInfo from '../components/UserInfo/UserInfo.jsx';
 import Disqus from '../components/Disqus/Disqus.jsx';
 import './atom-one-dark.css';
@@ -10,7 +11,8 @@ import './post.scss';
 export default class PostTemplate extends React.Component {
   render() {
     console.log(this.props);
-    const post = this.props.data.markdownRemark;
+    const postNode = this.props.data.markdownRemark;
+    const post = postNode.frontmatter;
     const config = this.props.data.site.siteMetadata;
     if (!post.id) {
       post.id = this.props.location.pathname;
@@ -19,20 +21,23 @@ export default class PostTemplate extends React.Component {
       post.category_id = config.postDefaultCategoryID;
     }
     return (
-      <div className="md-grid post-page-container">
+      <MainLayout SiteConfig={config} location={this.props.location.pathname}>
+        <div className="md-grid post-page-container">
 
-        <Helmet
-          title={`${post.frontmatter.title} | ${config.siteTitle}`}
-        />
-        <Card className="md-grid md-cell md-cell--12 post">
-          <CardText className="post-body">
-            <h1>{post.frontmatter.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: post.html }} />
-          </CardText>
-        </Card>
-        <UserInfo className="md-grid md-cell md-cell--12" SiteConfig={config} />
-        <Disqus post={post.frontmatter} disqusShortname={config.disqusShortname} />
-      </div>
+          <Helmet
+            title={`${post.title} | ${config.siteTitle}`}
+          />
+          <Card className="md-grid md-cell md-cell--12 post">
+            <CardText className="post-body">
+              <h1>{post.title}</h1>
+              <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
+            </CardText>
+          </Card>
+          <UserInfo className="md-grid md-cell md-cell--12" SiteConfig={config} />
+          <Disqus post={post} disqusShortname={config.disqusShortname} />
+        </div>
+      </MainLayout>
+
     );
   }
 }
@@ -49,8 +54,17 @@ query BlogPostBySlug($slug: String!) {
   }
 
   site {
-    siteMetadata {
-      siteTitle
+      siteMetadata {
+        siteTitle
+        linkPrefix
+        googleAnalyticsID
+        disqusShortname
+        postDefaultCategoryID
+        userName
+        userLocation
+        userAvatar
+        userDescription
+        copyright
     }
   }
 }
