1dfc477950d5f31a04aa76d1924b0bf17cd992d5
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-04 22:52:18 +0400

    Adds layout to the about page.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import Helmet from 'react-helmet';
+import MainLayout from '../components/MainLayout/MainLayout.jsx';
 import About from '../components/About/About.jsx';
 
 class AboutPage extends Component {
   render() {
+    const config = this.props.data.site.siteMetadata;
     return (
-      <div className="about-container">
-        {/* TODO: Set title from config.*/}
-        <About />
-      </div>
+      <MainLayout SiteConfig={config} location={this.props.location.pathname}>
+        <div className="about-container">
+          <Helmet title={`About | ${config.siteTitle}`} />
+          <About />
+        </div>
+      </MainLayout>
     );
   }
 }
 
 export default AboutPage;
+
+export const pageQuery = graphql`
+  query AboutQuery {
+    site {
+      siteMetadata {
+        siteTitle
+        linkPrefix
+        googleAnalyticsID
+        disqusShortname
+        postDefaultCategoryID
+        userName
+        userLocation
+        userAvatar
+        userDescription
+        copyright
+    }
+  }
+}
+`;
