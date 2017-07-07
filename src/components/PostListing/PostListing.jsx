092928d9d8c6c1b44f70c702c1d0152681aeed0f
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-07 20:34:05 +0400

    Makes PostListing a 12 column container.

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -18,7 +18,7 @@ class PostListing extends React.Component {
   render() {
     const postList = this.getPostList();
     return (
-      <div className="md-grid">
+      <div className="md-grid md-cell--12">
         {
           postList.map(post => (<PostPreview key={post.title} postInfo={post} />))
         }
