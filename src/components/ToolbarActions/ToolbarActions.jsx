a4d510a419792b2f806f9b0df3d177db7accc625
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-11 02:51:51 +0400

    Initial mockup.

--- src/components/ToolbarActions/ToolbarActions.jsx
+++ src/components/ToolbarActions/ToolbarActions.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import UserLinks from '../UserLinks/UserLinks.jsx';
 import './ToolbarActions.scss';
 
 class Toolbar extends Component {
   render() {
+    const { config } = this.props;
     return (
-      <div className="toolbar-children" />
+      <div className="toolbar-actions" >
+        <UserLinks config={config} />
+      </div>
     );
   }
 }
