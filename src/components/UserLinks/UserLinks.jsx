7bb2b6a30d705dba1194ad9d25a0b45c8b254d6b
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-10 16:47:53 +0400

    Fixes eslint warnings.

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -3,7 +3,6 @@ import Button from 'react-md/lib/Buttons';
 import './UserLinks.scss';
 
 class UserLinks extends Component {
-
   render() {
     const { userLinks } = this.props.SiteConfig;
     if (!userLinks) {
@@ -13,17 +12,16 @@ class UserLinks extends Component {
       <div className="user-links">
         {
           userLinks.map(link =>
-          (
-            <Button
+            (<Button
               icon
               secondary
               key={link.label}
               iconClassName={link.iconClassName}
               href={link.url}
             />
-          ),
+            ),
 
-        )
+          )
         }
       </div>
     );
