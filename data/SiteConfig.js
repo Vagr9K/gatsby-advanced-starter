658e34501b09f4e2f4f0109796a1310ffa4a030a
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-10 18:28:38 +0400

    Makes UserLocation more "random".

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   disqusShortname: 'https-vagr9k-github-io-gatsby-material-starter', // Disqus shortname.
   postDefaultCategoryID: 'Tech', // Default category for posts.
   userName: 'Material User', // Username to display in the author segment.
-  userLocation: 'Yerevan, Armenia', // User location to display in the author segment.
+  userLocation: 'North Pole, Earth', // User location to display in the author segment.
   userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
   userDescription: "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
