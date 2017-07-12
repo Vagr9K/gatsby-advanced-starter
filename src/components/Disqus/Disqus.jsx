9146c67aedb51f5fb62b50e701713fabad408419
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-12 15:51:20 +0400

    Now Disqus isn't being loaded by default on mobile devices.

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import ReactDisqusComments from 'react-disqus-comments';
-import Card from 'react-md/lib/Cards';
+import Card from 'react-md/lib/Cards/Card';
+import CardTitle from 'react-md/lib/Cards/CardTitle';
+import CardText from 'react-md/lib/Cards/CardText';
+import Avatar from 'react-md/lib/Avatars';
+import FontIcon from 'react-md/lib/FontIcons';
 import config from '../../../data/SiteConfig';
 
 class Disqus extends Component {
   render() {
-    const { post, onDisqusComment } = this.props;
+    const { post, onDisqusComment, expanded } = this.props;
     if (!config.disqusShortname) {
       return null;
     }
     return (
       <Card className="md-grid md-cell md-cell--12">
-        <ReactDisqusComments
-          shortname={config.disqusShortname}
-          identifier={post.id}
-          title={post.title}
-          url={post.url}
-          category_id={post.category_id}
-          onNewComment={onDisqusComment}
-        />
+        {
+          expanded ?
+            null
+            :
+            <CardTitle
+              title="Comments"
+              avatar={<Avatar icon={<FontIcon>comment</FontIcon>} />}
+              expander
+            />
+        }
+        <CardText expandable={!expanded}>
+          <ReactDisqusComments
+            shortname={config.disqusShortname}
+            identifier={post.id}
+            title={post.title}
+            url={post.url}
+            category_id={post.category_id}
+            onNewComment={onDisqusComment}
+          />
+        </CardText>
       </Card>
     );
   }
