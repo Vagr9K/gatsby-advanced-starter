da8e67a689681f904ea9c122781d64a46b0bebba
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-13 02:03:37 +0400

    Adds support for labeled UserLinks.

--- src/components/UserLinks/UserLinks.jsx
+++ src/components/UserLinks/UserLinks.jsx
@@ -5,17 +5,18 @@ import './UserLinks.scss';
 class UserLinks extends Component {
   getLinkElements() {
     const { userLinks } = this.props.config;
+    const { labeled } = this.props;
     return userLinks.map(link =>
       (<Button
-        icon
+        icon={!labeled}
+        flat={labeled}
         secondary
         key={link.label}
         iconClassName={link.iconClassName}
         href={link.url}
-      />
-      ),
-
-    );
+      >
+        {labeled ? link.label : ''}
+      </Button>));
   }
   render() {
     const { userLinks } = this.props.config;
