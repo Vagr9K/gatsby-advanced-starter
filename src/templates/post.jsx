65257f7cb1bda2dda174133611185c893a064ff0
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-12 14:33:57 +0400

    Initial redesign of post template.

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Card from 'react-md/lib/Cards';
 import CardText from 'react-md/lib/Cards/CardText';
+import CardTitle from 'react-md/lib/Cards/CardTitle';
+import Avatar from 'react-md/lib/Avatars';
+import FontIcon from 'react-md/lib/FontIcons';
+import Link from 'gatsby-link';
+import _ from 'lodash';
 import UserInfo from '../components/UserInfo/UserInfo.jsx';
 import Disqus from '../components/Disqus/Disqus.jsx';
 import PostTags from '../components/PostTags/PostTags.jsx';
@@ -21,21 +26,38 @@ export default class PostTemplate extends React.Component {
       post.category_id = config.postDefaultCategoryID;
     }
     return (
-      <div className="md-grid post-page-container">
-
+      <div className="post-page md-grid--no-spacing">
         <Helmet
           title={`${post.title} | ${config.siteTitle}`}
         />
-        <Card className="md-grid md-cell md-cell--12 post">
-          <CardText className="post-body">
-            <PostCover postNode={postNode} />
-            <h1 className="md-display-3 post-header">{post.title}</h1>
-            <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
-          </CardText>
-          <PostTags tags={post.tags} />
-        </Card>
-        <UserInfo className="md-grid md-cell md-cell--12" config={config} />
-        <Disqus post={post} />
+
+        <PostCover postNode={postNode} />
+        <div className="md-grid md-cell--9 post-page-contents">
+
+          <Card className="md-grid md-cell md-cell--12 post">
+            <CardText className="post-body">
+              <h1 className="md-display-3 post-header">{post.title}</h1>
+              <div className="post-info">
+                <CardTitle
+                  avatar={<Avatar icon={<FontIcon iconClassName="fa fa-calendar" />} />}
+                  title={`Published on ${post.date}`}
+                  subtitle={`${postNode.timeToRead} min read`}
+                />
+                <Link to={`/categories/${_.kebabCase(post.category)}`}>
+                  <CardTitle
+                    avatar={<Avatar icon={<FontIcon iconClassName="fa fa-folder-open" />} />}
+                    title={'In category'}
+                    subtitle={post.category}
+                  />
+                </Link>
+              </div>
+              <div dangerouslySetInnerHTML={{ __html: postNode.html }} />
+            </CardText>
+            <PostTags tags={post.tags} />
+          </Card>
+          <UserInfo className="md-grid md-cell md-cell--12" config={config} />
+          <Disqus post={post} />
+        </div>
       </div>
 
     );
