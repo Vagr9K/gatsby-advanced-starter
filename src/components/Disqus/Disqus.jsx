c79495a1e9c8351a36066a4e57ed0f209e8749a8
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-13 03:41:01 +0400

    Moves all comment notifications related functionality into Disqus component.

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -5,11 +5,30 @@ import CardTitle from 'react-md/lib/Cards/CardTitle';
 import CardText from 'react-md/lib/Cards/CardText';
 import Avatar from 'react-md/lib/Avatars';
 import FontIcon from 'react-md/lib/FontIcons';
+import Snackbar from 'react-md/lib/Snackbars';
 import config from '../../../data/SiteConfig';
 
 class Disqus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      toasts: [],
+    };
+    this.notifyAboutComment = this.notifyAboutComment.bind(this);
+    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
+  }
+
+  onSnackbarDismiss() {
+    const [, ...toasts] = this.state.toasts;
+    this.setState({ toasts });
+  }
+  notifyAboutComment() {
+    const toasts = this.state.toasts.slice();
+    toasts.push({ text: 'New comment available!' });
+    this.setState({ toasts });
+  }
   render() {
-    const { post, onDisqusComment, expanded } = this.props;
+    const { post, expanded } = this.props;
     if (!config.disqusShortname) {
       return null;
     }
@@ -27,9 +46,10 @@ class Disqus extends Component {
             title={post.title}
             url={post.url}
             category_id={post.category_id}
-            onNewComment={onDisqusComment}
+            onNewComment={this.notifyAboutComment}
           />
         </CardText>
+        <Snackbar toasts={this.state.toasts} onDismiss={this.onSnackbarDismiss} />
       </Card>
     );
   }
--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import Card from 'react-md/lib/Cards';
 import CardText from 'react-md/lib/Cards/CardText';
-import Snackbar from 'react-md/lib/Snackbars';
 import UserInfo from '../components/UserInfo/UserInfo.jsx';
 import Disqus from '../components/Disqus/Disqus.jsx';
 import PostTags from '../components/PostTags/PostTags.jsx';
@@ -18,11 +17,8 @@ export default class PostTemplate extends React.Component {
     super(props);
     this.state = {
       mobile: true,
-      toasts: [],
     };
     this.handleResize = this.handleResize.bind(this);
-    this.notifyAboutComment = this.notifyAboutComment.bind(this);
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
   }
   componentDidMount() {
     this.handleResize();
@@ -31,16 +27,6 @@ export default class PostTemplate extends React.Component {
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize);
-  }
-
-  onSnackbarDismiss() {
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
-  }
-  notifyAboutComment() {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text: 'New comment available!' });
-    this.setState({ toasts });
   }
 
   handleResize() {
@@ -84,8 +70,7 @@ export default class PostTemplate extends React.Component {
             </div>
           </Card>
           <UserInfo className="md-grid md-cell md-cell--12" config={config} expanded={expanded} />
-          <Disqus post={post} expanded={expanded} onDisqusComment={this.notifyAboutComment} />
-          <Snackbar toasts={this.state.toasts} onDismiss={this.onSnackbarDismiss} />
+          <Disqus post={post} expanded={expanded} />
         </div>
       </div>
 
