2db82b51e6f78be798dcc0469c32d0e21ec21f81
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-14 00:33:18 +0400

    Adds optional "Follow Me" widget in UserInfo.

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -5,6 +5,7 @@ import CardText from 'react-md/lib/Cards/CardText';
 import Avatar from 'react-md/lib/Avatars';
 import FontIcon from 'react-md/lib/FontIcons';
 import IconSeparator from 'react-md/lib/Helpers/IconSeparator';
+import { Follow } from 'react-twitter-widgets';
 import UserLinks from '../UserLinks/UserLinks.jsx';
 import './UserInfo.scss';
 
@@ -17,6 +18,7 @@ class UserInfo extends Component {
       userLocation,
       userDescription,
       userLinks,
+      userTwitter,
     } = this.props.config;
     const { expanded } = this.props;
     const userLinksElement = <UserLinks config={this.props.config} labeled={expanded} />;
@@ -36,7 +38,15 @@ class UserInfo extends Component {
           expander={!expanded}
           avatar={userAvatar && <Avatar src={userAvatar} role="presentation" />}
           title={userName && userName}
-          subtitle="Author"
+          subtitle={
+            userTwitter ?
+              <Follow
+                username={userTwitter}
+                options={{ count: expanded ? true : 'none' }}
+              />
+              :
+              'Author'
+          }
         />
         <CardText expandable={!expanded}>
           { userLocation &&
