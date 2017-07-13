0f055c2f1d259825a7e5ea753619956a892baebe
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-13 23:21:50 +0400

    Adds RSS link to the footer.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Button from 'react-md/lib/Buttons';
+import Link from 'gatsby-link';
 import UserLinks from '../UserLinks/UserLinks.jsx';
 import config from '../../../data/SiteConfig';
 import './Footer.scss';
 
 class Footer extends Component {
   render() {
+    const url = config.siteRss;
     const { copyright } = this.props.config;
     if (!copyright) {
       return null;
@@ -14,6 +17,10 @@ class Footer extends Component {
         <UserLinks config={config} labeled />
         <div className="notice-container">
           <h4>{copyright}</h4>
+
+          <Link to={url}>
+            <Button flat secondary iconClassName="fa fa-rss">Subscribe</Button>
+          </Link>
           <h4>
             Based on <a href="https://github.com/Vagr9K/gatsby-material-starter">Gatsby Material Starter</a>.
           </h4>
