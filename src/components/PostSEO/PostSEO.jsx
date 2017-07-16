45f139ce735477531c904560df5b02c730b829ce
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-17 01:57:05 +0400

    Initial implementation of Twitter SEO.

--- src/components/PostSEO/PostSEO.jsx
+++ src/components/PostSEO/PostSEO.jsx
@@ -52,6 +52,13 @@ class PostSEO extends Component {
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
         <meta property="fb:app_id" content={config.siteFBAppID ? config.siteFBAppID : ''} />
+
+        { /* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content={config.userTwitter ? config.userTwitter : ''} />
+        <meta name="twitter:title" content={title} />
+        <meta name="twitter:description" content={description} />
+        <meta name="twitter:image" content={image} />
       </Helmet>
     );
   }
