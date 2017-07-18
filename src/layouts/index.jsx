d748f08333d3e91f9b534476da332df7c6f199be
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-19 01:03:52 +0400

    Removes opionated layout parts.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import 'font-awesome/scss/font-awesome.scss';
-import Navigation from '../components/Navigation/Navigation';
 import config from '../../data/SiteConfig';
-import './index.scss';
-import './global.scss';
+import './index.css';
 
 export default class MainLayout extends React.Component {
   getLocalTitle() {
@@ -36,14 +33,13 @@ export default class MainLayout extends React.Component {
   render() {
     const { children } = this.props;
     return (
-      <Navigation config={config} LocalTitle={this.getLocalTitle()}>
-        <div>
-          <Helmet>
-            <meta name="description" content={config.siteDescription} />
-          </Helmet>
-          {children()}
-        </div>
-      </Navigation>
+      <div>
+        <Helmet>
+          <title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
+          <meta name="description" content={config.siteDescription} />
+        </Helmet>
+        {children()}
+      </div>
 
     );
   }
