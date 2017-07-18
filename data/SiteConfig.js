561984e3b499ccceb59c0ff82bc1cda4b2816992
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-19 00:39:40 +0400

    Removes opionated packages.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,16 +1,16 @@
 module.exports = {
   blogPostDir: 'sample-posts', // The name of directory that contains your posts.
-  siteTitle: 'Gatsby Material Starter', // Site title.
-  siteTitleAlt: 'GatsbyJS Material Starter', // Alternative site title for SEO.
+  siteTitle: 'Gatsby Advanced Starter', // Site title.
+  siteTitleAlt: 'GatsbyJS Advanced Starter', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
   siteUrl: 'https://vagr9k.github.io', // Domain of your website without pathPrefix.
-  pathPrefix: '/gatsby-material-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
-  siteDescription: 'A GatsbyJS stater with Material design in mind.', // Website description used for RSS feeds/meta description tag.
+  pathPrefix: '/gatsby-Advanced-starter', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
+  siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
   siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  disqusShortname: 'https-vagr9k-github-io-gatsby-material-starter', // Disqus shortname.
+  disqusShortname: 'https-vagr9k-github-io-gatsby-advanced-starter', // Disqus shortname.
   postDefaultCategoryID: 'Tech', // Default category for posts.
-  userName: 'Material User', // Username to display in the author segment.
+  userName: 'Advanced User', // Username to display in the author segment.
   userTwitter: '', // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: 'North Pole, Earth', // User location to display in the author segment.
   userAvatar: 'https://api.adorable.io/avatars/150/test.png', // User avatar to display in the author segment.
@@ -19,7 +19,7 @@ module.exports = {
   userLinks: [
     {
       label: 'GitHub',
-      url: 'https://github.com/Vagr9K/gatsby-material-starter',
+      url: 'https://github.com/Vagr9K/gatsby-advanced-starter',
       iconClassName: 'fa fa-github',
     },
     {
@@ -33,7 +33,7 @@ module.exports = {
       iconClassName: 'fa fa-envelope',
     },
   ],
-  copyright: 'Copyright © 2017. Material User', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © 2017. Advanced User', // Copyright string for the footer of the website and RSS feed.
 
 };
 
