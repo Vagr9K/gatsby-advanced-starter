2e4b0a0975990fb301fa0c17ccec5133ac95a394
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-07-28 00:05:16 +0400

    Fixes pathPrefix breaking sitemap urls.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,17 @@
 const config = require("./data/SiteConfig");
 
+const pathPrefix = config.pathPrefix === "/" ? "" : config.pathPrefix;
+
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    siteUrl: config.siteUrl + config.pathPrefix,
+    siteUrl: config.siteUrl + pathPrefix,
     rssMetadata: {
-      site_url: config.siteUrl + config.pathPrefix,
-      feed_url: config.siteUrl + config.pathPrefix + config.siteRss,
+      site_url: config.siteUrl + pathPrefix,
+      feed_url: config.siteUrl + pathPrefix + config.siteRss,
       title: config.siteTitle,
       description: config.siteDescription,
-      image_url: `${config.siteUrl + config.pathPrefix}/logos/logo-512.png`,
+      image_url: `${config.siteUrl + pathPrefix}/logos/logo-512.png`,
       author: config.userName,
       copyright: config.copyright
     }
