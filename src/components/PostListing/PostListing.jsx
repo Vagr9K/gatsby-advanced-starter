6aeee57212c623aa562f775ea25abc32c3be1e1b
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2017-08-05 03:39:48 +0400

    Fixes #6.

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -23,7 +23,7 @@ class PostListing extends React.Component {
       <div>
         {/* Your post list here. */
         postList.map(post =>
-          <Link to={post.path}>
+          <Link to={post.path} key={post.title}>
             <h1>
               {post.title}
             </h1>
