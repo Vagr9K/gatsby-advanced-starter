375014342d8a594835622754fc4286622a6c7a04
John <jonathan.braat@live.com>
2018-06-08 23:42:35 +0200

    fix date sorting issue

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,7 +19,7 @@ class Index extends React.Component {
 
 export default Index;
 
-/* eslint no-undef: "off"*/
+/* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query IndexQuery {
     allMarkdownRemark(
--- src/templates/tag.jsx
+++ src/templates/tag.jsx
@@ -16,7 +16,7 @@ export default class TagTemplate extends React.Component {
   }
 }
 
-/* eslint no-undef: "off"*/
+/* eslint no-undef: "off" */
 export const pageQuery = graphql`
   query TagPage($tag: String) {
     allMarkdownRemark(
