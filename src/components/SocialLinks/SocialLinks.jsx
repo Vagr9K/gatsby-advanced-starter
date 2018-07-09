208ad126c2a2bc7b26153c65d509c9a7022b5eb8
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-09 18:50:10 +0400

    Update SocialLinks.

--- src/components/SocialLinks/SocialLinks.jsx
+++ src/components/SocialLinks/SocialLinks.jsx
@@ -26,16 +26,18 @@ class SocialLinks extends Component {
     const { postNode, postPath, mobile } = this.props;
     const post = postNode.frontmatter;
     const url = urljoin(config.siteUrl, config.pathPrefix, postPath);
-
     const iconSize = mobile ? 36 : 48;
     const filter = count => (count > 0 ? count : "");
+    const renderShareCount = count => (
+      <div className="share-count">{filter(count)}</div>
+    );
 
     return (
       <div className="social-links">
         <RedditShareButton url={url} title={post.title}>
           <RedditIcon round size={iconSize} />
           <RedditShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
+            {count => renderShareCount(count)}
           </RedditShareCount>
         </RedditShareButton>
         <TwitterShareButton url={url} title={post.title}>
@@ -44,13 +46,13 @@ class SocialLinks extends Component {
         <GooglePlusShareButton url={url}>
           <GooglePlusIcon round size={iconSize} />
           <GooglePlusShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
+            {count => renderShareCount(count)}
           </GooglePlusShareCount>
         </GooglePlusShareButton>
         <FacebookShareButton url={url} quote={postNode.excerpt}>
           <FacebookIcon round size={iconSize} />
           <FacebookShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
+            {count => renderShareCount(count)}
           </FacebookShareCount>
         </FacebookShareButton>
         <LinkedinShareButton
@@ -60,7 +62,7 @@ class SocialLinks extends Component {
         >
           <LinkedinIcon round size={iconSize} />
           <LinkedinShareCount url={url}>
-            {count => <div className="share-count">{filter(count)}</div>}
+            {count => renderShareCount(count)}
           </LinkedinShareCount>
         </LinkedinShareButton>
         <TelegramShareButton url={url}>
