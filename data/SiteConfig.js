f835b9f012601407d3f280361851c8be18f07072
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-09 17:55:30 +0400

    Remove blogPostDir variable.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,5 +1,4 @@
 module.exports = {
-  blogPostDir: "sample-posts", // The name of directory that contains your posts.
   siteTitle: "Gatsby Advanced Starter", // Site title.
   siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
