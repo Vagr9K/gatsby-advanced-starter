bf500052fa702cced13b48da200f87b22e180f9c
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2018-07-09 17:42:30 +0400

    Update gatsby-node to properly use data formatting.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,52 @@
 const path = require("path");
 const _ = require("lodash");
 const webpackLodashPlugin = require("lodash-webpack-plugin");
+const moment = require("moment");
+const siteConfig = require("./data/SiteConfig");
+
+const postNodes = [];
+
+function addSiblingNodes(createNodeField) {
+  postNodes.sort(
+    ({ frontmatter: { date: date1 } }, { frontmatter: { date: date2 } }) => {
+      const dateA = moment(date1, siteConfig.dateFromFormat);
+      const dateB = moment(date2, siteConfig.dateFromFormat);
+
+      if (dateA.isBefore(dateB)) return 1;
+
+      if (dateB.isBefore(dateA)) return -1;
+
+      return 0;
+    }
+  );
+  for (let i = 0; i < postNodes.length; i += 1) {
+    const nextID = i + 1 < postNodes.length ? i + 1 : 0;
+    const prevID = i - 1 > 0 ? i - 1 : postNodes.length - 1;
+    const currNode = postNodes[i];
+    const nextNode = postNodes[nextID];
+    const prevNode = postNodes[prevID];
+    createNodeField({
+      node: currNode,
+      name: "nextTitle",
+      value: nextNode.frontmatter.title
+    });
+    createNodeField({
+      node: currNode,
+      name: "nextSlug",
+      value: nextNode.fields.slug
+    });
+    createNodeField({
+      node: currNode,
+      name: "prevTitle",
+      value: prevNode.frontmatter.title
+    });
+    createNodeField({
+      node: currNode,
+      name: "prevSlug",
+      value: prevNode.fields.slug
+    });
+  }
+}
 
 exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
   const { createNodeField } = boundActionCreators;
@@ -20,13 +66,32 @@ exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
     } else {
       slug = `/${parsedFilePath.dir}/`;
     }
-    if (
-      Object.prototype.hasOwnProperty.call(node, "frontmatter") &&
-      Object.prototype.hasOwnProperty.call(node.frontmatter, "slug")
-    ) {
-      slug = `/${_.kebabCase(node.frontmatter.slug)}`;
+
+    if (Object.prototype.hasOwnProperty.call(node, "frontmatter")) {
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "slug"))
+        slug = `/${_.kebabCase(node.frontmatter.slug)}`;
+      if (Object.prototype.hasOwnProperty.call(node.frontmatter, "date")) {
+        const date = moment(node.frontmatter.date, siteConfig.dateFromFormat);
+        if (!date.isValid)
+          console.warn(`WARNING: Invalid date.`, node.frontmatter);
+
+        createNodeField({
+          node,
+          name: "date",
+          value: date.toISOString()
+        });
+      }
     }
     createNodeField({ node, name: "slug", value: slug });
+    postNodes.push(node);
+  }
+};
+
+exports.setFieldsOnGraphQLNodeType = ({ type, boundActionCreators }) => {
+  const { name } = type;
+  const { createNodeField } = boundActionCreators;
+  if (name === "MarkdownRemark") {
+    addSiblingNodes(createNodeField);
   }
 };
 
