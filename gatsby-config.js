875aefde7c7897f74b71ed8382f4cf854b4d8f6c
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-01-21 02:25:19 +0400

    Fix incorrect RSS generator name.

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,7 +101,7 @@ module.exports = {
         setup(ref) {
           const ret = ref.query.site.siteMetadata.rssMetadata;
           ret.allMarkdownRemark = ref.query.allMarkdownRemark;
-          ret.generator = "GatsbyJS Material Starter";
+          ret.generator = "GatsbyJS Advanced Starter";
           return ret;
         },
         query: `
