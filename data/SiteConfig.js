06d022a39118048837be9ed4fae54ce9158381c3
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-01-21 02:03:31 +0400

    Add validation to SiteConfig.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,4 +1,4 @@
-module.exports = {
+const config = {
   siteTitle: "Gatsby Advanced Starter", // Site title.
   siteTitleShort: "GA Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: "GatsbyJS Advanced Starter", // Alternative site title for SEO.
@@ -41,3 +41,23 @@ module.exports = {
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
+
+// Validate
+
+// Make sure pathPrefix is empty if not needed
+if (config.pathPrefix === "/") {
+  config.pathPrefix = "";
+} else {
+  // Make sure pathPrefix only contains the first forward slash
+  config.pathPrefix = `/${config.pathPrefix.replace(/^\/|\/$/g, "")}`;
+}
+
+// Make sure siteUrl doesn't have an ending forward slash
+if (config.siteUrl.substr(-1) === "/")
+  config.siteUrl = config.siteUrl.slice(0, -1);
+
+// Make sure siteRss has a starting forward slash
+if (config.siteRss && config.siteRss[0] !== "/")
+  config.siteRss = `/${config.siteRss}`;
+
+module.exports = config;
