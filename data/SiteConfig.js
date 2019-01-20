24e9341bcfec794f920d5f685670d102b0bdc60c
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2019-01-21 02:32:42 +0400

    Update copyright dates.

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -37,7 +37,7 @@ const config = {
       iconClassName: "fa fa-envelope"
     }
   ],
-  copyright: "Copyright © 2017. Advanced User", // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2019. Advanced User", // Copyright string for the footer of the website and RSS feed.
   themeColor: "#c62828", // Used for setting manifest and progress theme colors.
   backgroundColor: "#e0e0e0" // Used for setting manifest background color.
 };
