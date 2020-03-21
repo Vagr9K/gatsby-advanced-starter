4d648bbb21fc2e1600822f93364acb9c2c809b62
David House <david.house@butcherbox.com>
2020-03-21 14:02:14 -0400

    Render a header component

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import { ThemeProvider, Box, CSSReset } from '@chakra-ui/core'
+import Header from '../components/Header'
 import config from '../../data/SiteConfig'
-import { ThemeProvider } from '@chakra-ui/core'
 import theme from '../theme'
-import './index.css'
 
 export default ({ children }) => {
   console.log('theme is', theme)
   return (
-    <div className="layout-container">
+    <>
       <Helmet>
         <meta name="description" content={config.siteDescription} />
         <html lang="en" />
       </Helmet>
-      <ThemeProvider theme={theme}>{children}</ThemeProvider>
-    </div>
+      <ThemeProvider theme={theme}>
+        <CSSReset />
+        <Box>
+          <Header />
+          {children}
+        </Box>
+      </ThemeProvider>
+    </>
   )
 }
