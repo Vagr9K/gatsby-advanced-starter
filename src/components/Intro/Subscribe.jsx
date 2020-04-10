d99ec70455dd670c6561b5a7130b7363652f830d
jonmccon <jonmccon@gmail.com>
2020-04-10 16:41:59 -0700

    changed rediret action to /#

--- src/components/Intro/Subscribe.jsx
+++ src/components/Intro/Subscribe.jsx
@@ -1,45 +1,12 @@
 import React from "react"
 
 export default class Subscribe extends React.Component {
-//   state = {
-//     email: "",
-//   }
-
-//   handleInputChange = event => {
-//     const target = event.target
-//     const value = target.value
-//     const name = target.name
-
-//     this.setState({
-//       [name]: value,
-//     })
-//   }
-
-
-// handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-// handleSubmit = e => {
-// fetch("/", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/x-www-form-urlencoded" },
-//     body: encode({ "form-name": "contact", ...this.state })
-// })
-//     .then(() => alert("Success!"))
-//     .catch(error => alert(error));
-
-// e.preventDefault();
-// };
-
-
-
-
-  
-  
 
   render() {
     return (
 
-        <form class="email-form" name="Subscribe" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+        // If a custom success page is needed, change the action attr here
+        <form class="email-form" name="Subscribe" method="POST" data-netlify="true" netlify-honeypot="bot-field" action="/#">
           
           <div hidden aria-hidden="true">
             <label>
@@ -58,19 +25,3 @@ export default class Subscribe extends React.Component {
     )
   }
 }
-
-{/* Subscribe form, uses Netlify ss function */}
-{/* <form class="email-form" name="newsletter" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-<div hidden aria-hidden="true">
-  <label>
-    Don’t fill this out if you're human: 
-    <input name="bot-field" />
-  </label>
-</div>
-<label for="email">Your email address</label>
-<div>
-  <input type="email" name="email" placeholder="Email"  id="email" required />
-  <input type="hidden" value="python" name="embeddedFormNetlify" />
-  <button type="submit">Subscribe</button>
-</div>
-</form> */}
