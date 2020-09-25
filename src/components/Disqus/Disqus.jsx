cb0e095e56405f8187e88a247431b1302ca753b3
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2020-09-25 15:04:17 +0400

    Refactor Disqus component to be hook friendly.

--- src/components/Disqus/Disqus.jsx
+++ src/components/Disqus/Disqus.jsx
@@ -1,51 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ReactDisqusComments from "react-disqus-comments";
 import urljoin from "url-join";
 import config from "../../../data/SiteConfig";
 
-class Disqus extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      toasts: [],
-    };
-    this.notifyAboutComment = this.notifyAboutComment.bind(this);
-    this.onSnackbarDismiss = this.onSnackbarDismiss.bind(this);
+function Disqus({ postNode }) {
+  const [toasts, setToasts] = useState([]);
+
+  function notifyAboutComment() {
+    const newToasts = toasts.slice();
+    newToasts.push({ text: "New comment available!" });
+    setToasts(newToasts);
   }
 
-  onSnackbarDismiss() {
-    const [, ...toasts] = this.state.toasts;
-    this.setState({ toasts });
+  if (!config.disqusShortname) {
+    return null;
   }
-
-  notifyAboutComment() {
-    const toasts = this.state.toasts.slice();
-    toasts.push({ text: "New comment available!" });
-    this.setState({ toasts });
-  }
-
-  render() {
-    const { postNode } = this.props;
-    if (!config.disqusShortname) {
-      return null;
-    }
-    const post = postNode.frontmatter;
-    const url = urljoin(
-      config.siteUrl,
-      config.pathPrefix,
-      postNode.fields.slug
-    );
-    return (
-      <ReactDisqusComments
-        shortname={config.disqusShortname}
-        identifier={post.title}
-        title={post.title}
-        url={url}
-        category_id={post.category_id || null}
-        onNewComment={this.notifyAboutComment}
-      />
-    );
-  }
+  const post = postNode.frontmatter;
+  const url = urljoin(config.siteUrl, config.pathPrefix, postNode.fields.slug);
+  return (
+    <ReactDisqusComments
+      shortname={config.disqusShortname}
+      identifier={post.title}
+      title={post.title}
+      url={url}
+      category_id={post.category_id || null}
+      onNewComment={notifyAboutComment}
+    />
+  );
 }
 
 export default Disqus;
