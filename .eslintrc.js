96a075d7d759360fcd11c4e1bb790627ec4de9d1
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2020-09-25 15:04:01 +0400

    Add React Hook linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
-  plugins: ["react", "prettier"],
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:react-hooks/recommended",
+  ],
+  plugins: ["react", "prettier", "react-hooks"],
   rules: { "react/prop-types": 0 },
 };
