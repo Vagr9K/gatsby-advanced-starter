f0ca36a00d46cf74560574de21a7d4e3c4af8a83
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-06-03 14:04:30 +0400

    Fix Jest file stubs not working.

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,7 @@ module.exports = {
   moduleNameMapper: {
     ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
     ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/file-mock.js",
+      "<rootDir>/test/__mocks__/file-mock.js",
   },
   testPathIgnorePatterns: [
     "node_modules",
