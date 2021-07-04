77b517e18acd6daeb8896de86bcb12bff8409c50
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-07-04 14:31:48 +0400

    Bump dependency versions.

--- gatsby-config.js
+++ gatsby-config.js
@@ -75,7 +75,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    "gatsby-plugin-catch-links",
     "gatsby-plugin-twitter",
     "gatsby-plugin-sitemap",
     {
