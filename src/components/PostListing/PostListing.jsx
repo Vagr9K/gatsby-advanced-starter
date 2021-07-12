c00cb0f6cb09bbfabeb12b3c629a3a752941ec9e
tngl <tngl@tngls-mac-mini.lan>
2021-07-11 23:40:22 -0400

    fix key mapping

--- src/components/PostListing/PostListing.jsx
+++ src/components/PostListing/PostListing.jsx
@@ -3,7 +3,7 @@ import { Link } from "gatsby";
 
 function PostListing({ postEdges }) {
   const postList = [];
-  postEdges.forEach((postEdge) => {
+  postEdges.forEach(postEdge => {
     postList.push({
       path: postEdge.node.fields.slug,
       tags: postEdge.node.frontmatter.tags,
@@ -11,63 +11,64 @@ function PostListing({ postEdges }) {
       title: postEdge.node.frontmatter.title,
       date: postEdge.node.fields.date,
       excerpt: postEdge.node.excerpt,
-      timeToRead: postEdge.node.timeToRead,
+      timeToRead: postEdge.node.timeToRead
     });
   });
   const squares = [
     {
       render: true,
-      index: 0,
+      index: 0
     },
     {
-      render: false,
+      render: false
     },
     {
-      render: false,
+      render: false
     },
     {
-      render: false,
-    },
-    {
-      render: true,
-      index: 1,
-    },
-    {
-      render: false,
-    },
-    {
-      render: false,
-    },
-    {
-      render: false,
+      render: false
     },
     {
       render: true,
-      index: 2,
+      index: 1
     },
+    {
+      render: false
+    },
+    {
+      render: false
+    },
+    {
+      render: false
+    },
+    {
+      render: true,
+      index: 2
+    }
   ];
 
   return (
     <div className="home-post-wrapper">
       {
         /* Your post list here. */
-        squares.map((square) =>
-          square.render ? (
+        squares.map((square, index) => {
+          const renderSquare = square.render;
+          const to = renderSquare && postList[square.index].path;
+          const key = renderSquare
+            ? postList[square.index].title
+            : `postListFiller${index}`;
+          const title = renderSquare && postList[square.index].title;
+
+          return square.render ? (
             <>
-              <Link
-                className="home-post-link"
-                to={postList[square.index].path}
-                key={postList[square.index].title}
-              >
-                <h1 className="home-post-title">
-                  {postList[square.index].title}
-                </h1>
+              <Link className="home-post-link" to={to} key={key}>
+                <h1 className="home-post-title">{title}</h1>
               </Link>
             </>
           ) : (
-            <div className="filler" />
-          )
-        )
+            <div className="filler" key={key} />
+          );
+        })
       }
     </div>
   );
