90b286aa61b7255a2b653b06c7aee430e54307c3
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-07-13 15:45:00 +0400

    Properly export the components/types of gatsby-theme-amaranth.

---
+++ themes/amaranth/src/components/shared/index.ts
@@ -0,0 +1,4 @@
+export { default as Image } from "./Image";
+export { default as LayoutWidthContainer } from "./LayoutWidthContainer";
+export { default as ListingPageWrapper } from "./ListingPageWrapper";
+export { default as Separator } from "./Separator";
