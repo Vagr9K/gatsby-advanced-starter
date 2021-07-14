48c710605b0e85aea4a19f3ecf459be2a79b5561
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-07-14 16:50:38 +0400

    Improve the styling of the advanced theme demo.

--- examples/advanced-demo/src/components/FeedListing/index.tsx
+++ examples/advanced-demo/src/components/FeedListing/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 
 import { Link, Types } from "gatsby-theme-advanced";
 
+import { getImage, GatsbyImage } from "gatsby-plugin-image";
+
 import "./styles.css";
 
 type PostListingProps = {
@@ -15,10 +17,25 @@ const FeedListing = ({ listing }: PostListingProps): JSX.Element => (
       if ("isPlaceholder" in feedItem)
         return <h1 key={feedItem.key}>Loading...</h1>;
 
+      const { coverImg } = feedItem;
+      if (!coverImg)
+        throw Error("Article: Failed to render Article without cover image.");
+
+      const gatsbyImage = getImage(coverImg);
+
+      if (!gatsbyImage)
+        throw Error(
+          "Article: Failed to get Gatsby image data from the cover image."
+        );
+
       return (
-        <Link to={feedItem.route} key={feedItem.slug}>
-          <h1> {feedItem.title}</h1>
-        </Link>
+        <React.Fragment key={`${feedItem.slug}`}>
+          <Link to={feedItem.route}>
+            <h1> {feedItem.title}</h1>
+            <GatsbyImage image={gatsbyImage} alt={feedItem.coverImageAlt} />
+          </Link>
+          <p>{feedItem.excerpt}</p>
+        </React.Fragment>
       );
     })}
   </div>
