98458c3349c55a6f0c3121e2471403b428c654f7
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-07-26 16:09:49 +0400

    Add tests for the Feed template.

---
+++ themes/advanced/src/templates/feed/tests/FeedTemplate.spec.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+import renderWithContext from "../../../../test/render";
+
+import PostTemplate from "../index";
+
+import { FeedPageMetaFromJson } from "../../../types";
+import { PageContext } from "../types";
+import Index0 from "../../../../test/fixtures/feedMetadata/index-0.json";
+
+const indexFeedContext: PageContext = {
+  feedId: "test",
+  feedPageMeta: Index0 as unknown as FeedPageMetaFromJson,
+  feedType: "index",
+  pageCount: 3,
+  pageIndex: 0,
+};
+
+const categoryFeedContext: PageContext = {
+  feedId: "test",
+  feedPageMeta: Index0 as unknown as FeedPageMetaFromJson,
+  feedType: "category",
+  pageCount: 3,
+  pageIndex: 0,
+};
+
+const tagFeedContext: PageContext = {
+  feedId: "test",
+  feedPageMeta: Index0 as unknown as FeedPageMetaFromJson,
+  feedType: "tag",
+  pageCount: 3,
+  pageIndex: 0,
+};
+
+describe("component FeedTemplate", () => {
+  it("correctly render an index feed", () => {
+    expect.assertions(1);
+
+    const { asFragment } = renderWithContext(
+      <PostTemplate pageContext={indexFeedContext} />
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("correctly render a tag feed", () => {
+    expect.assertions(2);
+
+    const { asFragment } = renderWithContext(
+      <PostTemplate pageContext={tagFeedContext} />
+    );
+
+    const helmet = Helmet.peek();
+
+    expect(helmet.title).toStrictEqual(
+      'Posts tagged as "test" | Gatsby Advanced Starter'
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("correctly render a category feed", () => {
+    expect.assertions(2);
+
+    const { asFragment } = renderWithContext(
+      <PostTemplate pageContext={categoryFeedContext} />
+    );
+
+    const helmet = Helmet.peek();
+
+    expect(helmet.title).toStrictEqual(
+      'Posts in category "test" | Gatsby Advanced Starter'
+    );
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
