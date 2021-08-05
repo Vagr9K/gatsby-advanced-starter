58b43f958879618cf9d6234b35a55ebb4920a8ac
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-08-06 03:36:45 +0400

    Update default gatsby-theme-advanced configuration.

--- examples/amaranth-demo/gatsby-config.js
+++ examples/amaranth-demo/gatsby-config.js
@@ -3,7 +3,36 @@ module.exports = {
     "gatsby-plugin-styled-components",
     {
       resolve: "gatsby-theme-amaranth",
-      options: {},
+      options: {
+        website: {
+          fbAppId: "1825356251115265",
+          twitterName: "Vagr9K",
+          url: "https://gatsby-advanced-starter-demo.netlify.com",
+          googleAnalyticsId: "UA-47311644-5",
+          copyright: "© Copyright 2021 | Ruben Harutyunyan",
+          disqusShortname: "https-vagr9k-github-io-gatsby-advanced-starter",
+        },
+
+        user: {
+          id: "AdvancedUser",
+          firstName: "Advanced",
+          lastName: "User",
+          twitterName: "Vagr9K",
+          linkedIn: "your-linkedin",
+          github: "vagr9k",
+          email: "AdvancedUser@example.com",
+          location: "User Location",
+          about: "A full-stack web developer looking for a challenge!",
+          avatar: "https://i.pravatar.cc/300",
+        },
+
+        organization: {
+          name: "Organization Name",
+          description: "Organization description",
+          logoUrl: "/logos/logo-512.png",
+          url: "https://gatsby-advanced-starter-demo.netlify.com",
+        },
+      },
     },
   ],
 };
--- themes/advanced/src/config/defaultConfig.ts
+++ themes/advanced/src/config/defaultConfig.ts
@@ -9,40 +9,19 @@ const config: SiteConfig = {
     description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
     language: "en", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-    fbAppId: "1825356251115265", // FB Application ID for using app insights
-    twitterName: "Vagr9K", // Twitter handle of the website
-    url: "https://gatsby-advanced-starter-demo.netlify.com", // Domain of your website without the pathPrefix
+    url: "https://example.com", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
     rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-
-    googleAnalyticsId: "UA-47311644-5", // GA tracking ID
-    copyright: "© Copyright 2021 | Ruben Harutyunyan", // Copyright string for the footer of the website and RSS feed.
-
+    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
     backgroundColor: "#F7F7F7", // Used for setting manifest background color.
   },
 
   // User configuration
-  user: {
-    id: "AdvancedUser", // Unique identifier of the user on the website. User for OpenGraph SEO tags
-    firstName: "Advanced", // Used for SEO
-    lastName: "User", // Used for SEO
-    twitterName: "Vagr9K", // Twitter username used for SEO
-    linkedIn: "your-linkedin", // Used for contact information
-    github: "vagr9k", // Used for contact information
-    email: "AdvancedUser@example.com", // Used for contact information and in the RSS feed
-    location: "User Location", // User location used for SEO
-    about: "A full-stack web developer looking for a challenge!", // User information used for the author section
-    avatar: "https://i.pravatar.cc/300", // User avatar used for the author section
-  },
+  user: undefined,
 
   // Organization information used for SEO
-  organization: {
-    name: "Organization Name",
-    description: "Organization description",
-    logoUrl: "/logos/logo-512.png",
-    url: "https://gatsby-advanced-starter-demo.netlify.com", // URL of the organization website
-  },
+  organization: undefined,
 
   // Gatsby Configuration
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
