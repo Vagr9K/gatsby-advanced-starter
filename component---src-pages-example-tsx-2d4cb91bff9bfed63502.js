04534b88f0246cbedeca26ab8e0eb639c38ee621
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-08-07 02:21:35 +0400

    Updates

--- component---src-pages-example-tsx-2d4cb91bff9bfed63502.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_advanced=self.webpackChunkgatsby_starter_advanced||[]).push([[168],{7919:function(e,t,a){"use strict";a.r(t);var n=a(7294);t.default=function(){return n.createElement("h1",null,"Sample content")}}}]);
-//# sourceMappingURL=component---src-pages-example-tsx-2d4cb91bff9bfed63502.js.map
--- component---themes-amaranth-src-pages-about-tsx-ed16ee35669938f52798.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkgatsby_starter_advanced=self.webpackChunkgatsby_starter_advanced||[]).push([[467],{3853:function(e,t,a){"use strict";a.r(t);var n=a(7294),r=a(5414),s=a(4241),c=a(8077);t.default=function(){var e=n.useContext(s.E_);return n.createElement(c.Z,null,n.createElement("div",{className:"about-container"},n.createElement(r.q,{title:"About | "+e.website.title})))}}}]);
-//# sourceMappingURL=component---themes-amaranth-src-pages-about-tsx-ed16ee35669938f52798.js.map
