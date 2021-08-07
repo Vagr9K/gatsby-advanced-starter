cbd4655bf0d643bf4362d4d089e8efa8021750af
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2021-08-07 15:41:50 +0400

    Fix gatsby-theme-advanced failing to work due to ts-node ignoring it as a package.

--- themes/advanced/gatsby-config.js
+++ themes/advanced/gatsby-config.js
@@ -2,7 +2,11 @@ require("source-map-support").install();
 require("ts-node").register({
   transpileOnly: true,
   files: true,
-  ignore: ["(?:^|/)node_modules/", "(?:^|/).cache/", "(?:^|/)public/"],
+  ignore: [
+    "(?:^|/)node_modules/(?!gatsby-theme-advanced)",
+    "(?:^|/).cache/",
+    "(?:^|/)public/",
+  ],
 });
 
 module.exports = require("./gatsby/gatsby-config");
