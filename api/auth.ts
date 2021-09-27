cc462493a92a32ff74a4d11794aae0377698254a
vasfvitor <vasfvitor@gmail.com>
2021-09-27 20:17:17 -0300

    fixed import module VERCEL

---
+++ api/auth.ts
@@ -0,0 +1,2 @@
+import {} from '@openlab/vercel-netlify-cms-github'
+export { auth as default } from '@openlab/vercel-netlify-cms-github'
--- api/callback.ts
+++ api/callback.ts
@@ -1,1 +1,2 @@
+import {} from '@openlab/vercel-netlify-cms-github'
 export { callback as default } from '@openlab/vercel-netlify-cms-github'
